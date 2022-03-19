017fd2f6c34e37845f3bca1546747202d13d69e7
Aquaman1234555 <101315879+Aquaman1234555@users.noreply.github.com>
2022-03-19 09:27:21 +0530

    Add files via upload

---
+++ scratch_canvas (5).py
@@ -0,0 +1,8 @@
+side = int(input('enter the side value '))
+print('''
+The perimeter of square is : ''', peri_sq, '''
+The area of square is : ''', area_sq)
+side = int(input('enter the side value '))
+print('''
+The perimeter of square is : ''', peri_sq, '''
+The area of square is : ''', area_sq)
